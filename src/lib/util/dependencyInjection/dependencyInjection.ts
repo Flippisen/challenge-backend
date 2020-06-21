5ee7d39f2a8cf3217930771b774b5161847ed554
Philip Nilsson <philip@nilsson.gi>
2020-06-21 14:14:10 +0100

    Added challenge logic to dependency injection

--- src/lib/util/dependencyInjection/dependencyInjection.ts
+++ src/lib/util/dependencyInjection/dependencyInjection.ts
@@ -1,4 +1,5 @@
 export const DependencyInjection = {
+  ChallengeLogic: Symbol.for('ChallengeLogic'),
   ChallengeRepository: Symbol.for('ChallengeRepository'),
   ConnectionProvider: Symbol.for('ConnectionProvider'),
 };
--- src/lib/util/dependencyInjection/inversify.config.ts
+++ src/lib/util/dependencyInjection/inversify.config.ts
@@ -4,8 +4,10 @@ import { DependencyInjection } from './dependencyInjection';
 import { DatabaseConnectionProvider } from '../databaseConnectionProvider';
 import { ChallengeRepository } from '../../repositories/interfaces/challengeRepository';
 import { MongoChallengeRepository } from '../../repositories/mongoChallengeRepository';
+import { ChallengeLogic } from '../../logic/challengeLogic';
 
 const injector = new Container();
+injector.bind<ChallengeLogic>(DependencyInjection.ChallengeLogic).to(ChallengeLogic);
 injector.bind<ChallengeRepository>(DependencyInjection.ChallengeRepository).to(MongoChallengeRepository);
 injector.bind<ConnectionProvider>(
     DependencyInjection.ConnectionProvider).to(DatabaseConnectionProvider);
